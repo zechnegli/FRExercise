6ad0f15ab87ea9be9664d7bd4dc60273ce22aa8f
jeremy <zechengli1997@gmail.com>
2021-03-08 02:13:32 -0800

    remove pods

--- Pods/Target Support Files/Pods-FetchRewardsZHL-FetchRewardsZHLUITests/Pods-FetchRewardsZHL-FetchRewardsZHLUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FetchRewardsZHL_FetchRewardsZHLUITests : NSObject
-@end
-@implementation PodsDummy_Pods_FetchRewardsZHL_FetchRewardsZHLUITests
-@end
